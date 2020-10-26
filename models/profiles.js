c30a2e5710ac5c829f45d5c446baf5563cc3c226
tachyon <83tachyon@gmail.com>
2020-10-26 18:04:02 +0900

    profiles extended

--- models/profiles.js
+++ models/profiles.js
@@ -29,5 +29,35 @@ var profiles = {
         banList: [],
         socket_id: null,
     }
+    mary: {
+        id: 'mary',
+        pw: 111,
+        nick: 'cruz',
+        img: null,
+        status: 0,
+        friendsList: [],
+        banList: [],
+        socket_id: null,
+    }
+    jane: {
+        id: 'jane',
+        pw: 111,
+        nick: 'cruz',
+        img: null,
+        status: 0,
+        friendsList: [],
+        banList: [],
+        socket_id: null,
+    }
+    alice: {
+        id: 'alice',
+        pw: 111,
+        nick: 'cruz',
+        img: null,
+        status: 0,
+        friendsList: [],
+        banList: [],
+        socket_id: null,
+    }
 };
 module.exports = profiles
