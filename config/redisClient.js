94fef9daf27241ae68c3e440ede07f9f6abb9b38
tachyon <83tachyon@gmail.com>
2020-10-31 07:21:38 +0900

    password corrected

--- config/redisClient.js
+++ config/redisClient.js
@@ -3,7 +3,7 @@ const redis = require('redis')
 // const redisClient = redis.createClient(redisSetting.port, redisSetting.host)
 const redisClient = redis.createClient(process.env.REDIS_URL || { port: redisSetting.port, host: redisSetting.host })
 // const redisClient = redis.createClient(process.env.REDIS_URL)
-const REDIS_Password = pa1fa3945bc6286338291f048bd62b3987813248b28bd3af99c34b53e40563ddd
+const REDIS_Password = 'pa1fa3945bc6286338291f048bd62b3987813248b28bd3af99c34b53e40563ddd'
 redisClient.auth(REDIS_Password, function (err) {
     if (err) throw err
 })
