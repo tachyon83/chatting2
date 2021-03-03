bfda820ae0da064ece99fb8c4f7d17f7a5ed5b1c
tachyon <83tachyon@gmail.com>
2021-03-03 23:56:37 +0900

    from id to username. restored.

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -49,8 +49,7 @@ module.exports = () => {
         // done(null, id);
     })
     passport.use(new LocalStrategy({
-        // usernameField: 'username',
-        usernameField: 'id',
+        usernameField: 'username',
         passwordField: 'password',
         session: true, //세션에 저장 여부
         // passReqToCallback:false,
